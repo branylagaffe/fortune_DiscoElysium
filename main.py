a3e63b5069bc6650fd445deecdc6ed7760cc7970
Bryan `Brany` Perdrizat <bryan.perdrizat@gmail.com>
2022-09-11 15:03:37 +0200

    Use auto loading

--- main.py
+++ main.py
@@ -2,10 +2,25 @@
 
 from os import path
 from sqlalchemy import create_engine
+from sqlalchemy.ext.automap import automap_base, classname_for_table
+from sqlalchemy.orm import Session
 
 
 
 DIR = path.dirname(path.realpath(__file__))
-DB = path.join(DIR, 'databases', 'sqlite', 'disco.sqlite3')
+DB  = path.relpath(path.join(DIR, 'databases', 'sqlite', 'discobase12-17-2021-4-18-51-PM.db'), DIR)
 
-engine = create_engine("sqlite://{}".format(DB), echo=True, future=True)
+engine = create_engine("sqlite:///{}".format(DB), echo=True, future=True)
+
+Base = automap_base()
+Base.prepare(autoload_with=engine)
+
+Actors = Base.classes.actors
+Dialogues = Base.classes.dialogues
+
+session = Session(engine)
+
+a_actor = session.query(Actors).first()
+c = session.query(Dialogues).first()
+
+print(c.description)
